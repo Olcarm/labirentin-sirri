6c54829c243e788cdb34e1f18e2661ade5187c47
TugbaKır <132365770+tugbaaKir@users.noreply.github.com>
2023-12-09 15:53:36 +0300

    ehtjhrgthyk

---
+++ Labirentin-Sirri/Assets/Scripts/ImageCanvas.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ImageCanvas : MonoBehaviour
+{
+    public TextMeshProUGUI resultText;
+    public List<Deference> buttons = new List<Deference>(); 
+    public Image img1;
+    public Image img2;
+
+    public Sprite[] firstImages;
+    public Sprite[] secondImages;
+
+
+    public int inc = 0;
+    public void ChangeImages()
+    {
+        if(inc < firstImages.Length)
+        {
+            img1.sprite = firstImages[inc];
+            img2.sprite = secondImages[inc];
+            ChangeTexts();
+        }
+       
+    }
+
+    private void ChangeTexts()
+    {
+        foreach(Deference d in buttons)
+        {
+            d.ChangeTexts();
+        }
+    }
+
+    public void ShowResult(bool isTrue)
+    {
+        if (isTrue)
+            resultText.text = "DOÐRU";
+        
+        else
+            resultText.text = "YANLIÞ";
+
+    }
+}
